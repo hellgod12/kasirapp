6b33261fa722d8ed5cdf24396e0cddd9dc97a2e8
kenayayummy <hellgodhellfuckinggod@gmail.com>
2026-06-07 03:16:20 +0700

    Fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+export default nextConfig;
